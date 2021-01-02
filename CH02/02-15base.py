b85ad0a4f4d1615b72b3f2652581a4a5a7b593f6
hansolj991 <hansolj991@naver.com>
2021-01-03 07:55:48 +0900

    2020-12-28

---
+++ CH02/02-15base.py
@@ -0,0 +1,6 @@
+data = int(input('정수 입력 >>'))
+
+print('2진수: ', bin(data))
+print('8진수: ', oct(data))
+print('10진수: ', data)
+print('16진수: ', hex(data))
---
+++ CH02/02-16hexsys.py
@@ -0,0 +1,7 @@
+invar = input('16진수 정수 입력 >> ')
+data = int(invar, 16)
+
+print('2진수: ', bin(data))
+print('8진수: ', oct(data))
+print('10진수: ', data)
+print('16진수: ', hex(data))
