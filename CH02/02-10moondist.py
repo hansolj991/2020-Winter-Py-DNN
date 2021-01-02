0b6dbf873a59be639fed1a7cc1011d3fd1b37f48
hansolj991 <hansolj991@naver.com>
2021-01-03 08:01:38 +0900

    2020-12-28

--- CH02/02-10moondist.py
+++
@@ -1,4 +0,0 @@
-distance = 384400
-unit = 10000
-manUnit, remainder = divmod(distance, unit)  #divmo(a,b) a와 b의 몫과 나머지 구할수 있음
-print('지구에서 달까지의 거리:' , manUnit, '만', remainder,'킬로미터')
--- CH02/02-15base.py
+++
@@ -1,6 +0,0 @@
-data = int(input('정수 입력 >>'))
-
-print('2진수: ', bin(data))
-print('8진수: ', oct(data))
-print('10진수: ', data)
-print('16진수: ', hex(data))
