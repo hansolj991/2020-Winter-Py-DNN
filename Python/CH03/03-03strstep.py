ac07e138b5b642738d63ba08e0aa09da3d3badf3
hansolj991 <hjs8170@naver.com>
2021-01-03 17:06:18 +0900

    2020-12-28

---
+++ Python/CH03/03-03strstep.py
@@ -0,0 +1,9 @@
+str = '일요일 기러기'
+print(str[:3], str[4:]) #양수이용
+print(str[:-4], str[-3:]) #음수이용
+print(str[:], str[::]) #모든 문자열 참조
+print(str[::2],) #한 문자씩 건너뛰기
+print(str[::3],) #두 문자씩 건너뛰기
+print(str[::-2],) #역순으로 한 문자씩 건너뛰기
+print(str[::-1],) #역순으로 출력
+
