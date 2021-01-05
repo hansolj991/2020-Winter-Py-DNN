b2664be6839ec927534542e6a16a4bca0c476630
hansolj991 <hjs8170@naver.com>
2021-01-06 01:39:33 +0900

    2020-12-29

---
+++ Python/CH04/04-08coffeeorder.py
@@ -0,0 +1,12 @@
+for i in range(3):
+    coffee = input("주문하세요! [아메리카노] [카페라테] [카푸치노] >>")
+    if coffee == '아메리카노':
+        print('%s 주문' % coffee)
+    elif coffee == '카페라테':
+        print('%s 주문' % coffee)
+    elif coffee == '카푸치노':
+        print('%s 주문 % coffee')
+    else:
+        print('모르겠어요.')
+else:
+    print('주문을 마치겠습니다.')
