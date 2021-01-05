e79fd7389783ff84e93ec46b9d9b1e0c20f5c9cf
hansolj991 <hjs8170@naver.com>
2021-01-06 06:25:48 +0900

    2020-12-29

--- Python/CH04/04-16dayspelltest.py
+++ Python/CH04/04-16dayspelltest.py
@@ -1,1 +1,10 @@
 days = ['monday', 'tuesday', 'wednesday']
+
+while True:
+    user = input('월, 화, 수 중 하나 영어 단어 입력 >>')
+    if user not in days:
+        print('잘못 입력했어요!')
+        continue
+    print('입력: %s, 철자가 맞습니다.' % user)
+    break
+print(' 종료 '.center(15, '*'))
