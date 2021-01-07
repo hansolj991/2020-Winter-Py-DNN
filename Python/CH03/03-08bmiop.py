3af1acca746e94337165081051fcab4dd419f4f6
hansolj991 <hjs8170@naver.com>
2021-01-07 22:22:40 +0900

    2020-12-28

---
+++ Python/CH03/03-08bmiop.py
@@ -0,0 +1,11 @@
+h, w = input('당신의 키(cm)와 몸무게(kg)는? >> ').split()
+height = float(h)
+weight = float(w)
+bmi = weight / (height/100)**2
+print('키:%6.1f(cm), 몸무게:%5.1f(km), BMI:%5.1f' % (height, weight, bmi))
+print('{} {}'.format('고도 비만', 40 <= bmi))
+print('{} {}'.format('중등도 비만', 35 <= bmi < 40))
+print('{} {}'.format('비만', 30 <= bmi < 35))
+print('{} {}'.format('과체중', 25 <= bmi < 30))
+print('{} {}'.format('정상', 18.5 <= bmi < 25))
+print('{} {}'.format('저체중', bmi < 18.5))
