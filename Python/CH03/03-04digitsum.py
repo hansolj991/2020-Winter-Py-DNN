6ee0190566429bab23a19ff7a93ef1f1844b4067
hansolj991 <hjs8170@naver.com>
2021-01-07 22:11:27 +0900

    2020-12-30

---
+++ Python/CH03/03-04digitsum.py
@@ -0,0 +1,10 @@
+vlaue = input('실수(세 자리.두 자리로 345.78처럼)를 하나 입력하세요. >>')
+num = vlaue.replace('.','')
+sum = 0
+sum += int(num[0])
+sum += int(num[1])
+sum += int(num[2])
+sum += int(num[3])
+sum += int(num[4])
+print('입력값:', vlaue)
+print('모든 자릿수 합:', sum)
