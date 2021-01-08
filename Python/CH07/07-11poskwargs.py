8054aa1b9d6a895528bc5b60881ba41ce6ab81d8
hansolj991 <hjs8170@naver.com>
2021-01-08 23:21:03 +0900

    2021-01-04

---
+++ Python/CH07/07-11poskwargs.py
@@ -0,0 +1,12 @@
+def sumvalue(value, **kwargs):
+    hap = value
+    for key in kwargs:
+        hap += kwargs[key]
+    return hap
+
+coffeeprice = {'에스프레소':2000, '아메리카노':2800, '카페라테': 3200}
+print(sumvalue(1000, **coffeeprice))
+print(sumvalue(value= 2000, **coffeeprice))
+print(sumvalue(**coffeeprice, value= 2000)) #둘다 키워드 인자
+
+
