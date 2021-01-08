f133f9b746001de4c3affb2067e8192fa75c48c6
hansolj991 <hjs8170@naver.com>
2021-01-08 23:00:15 +0900

    2021-01-04

---
+++ Python/CH07/07-05tempfuntion.py
@@ -0,0 +1,12 @@
+def ctofahrenhite(cels):
+    """인자인 섭씨 온도 cels을 화씨 온도로 변환해 반환"""
+    return cels * 9/5 + 32
+
+def ftocelsius(fahr):
+    """인자인 화씨 온도 fahr을 섭씨 온도로 변환해 반환"""
+    return (fahr - 32) * 5/9
+for cels in range(28, 35, 2):
+    print('섭씨: {}, 화씨: {:.2f}'.format(cels, ctofahrenhite(cels)))
+
+for fahr in range(90, 103, 3):
+    print('섭씨: {:.2f}, 화씨: {}'.format(ftocelsius(fahr), fahr))
