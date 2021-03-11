9076b125d3bd5f022d273b9dfad39b4a027be6a7
hansolj991 <66578282+hansolj991@users.noreply.github.com>
2021-03-11 10:14:11 +0900

    2021-3-11

--- Python/Challenge/CH02/2.py
+++ Python/Challenge/CH02/2.py
@@ -1,6 +1,6 @@
 # 킬로미터 단위로 거리를 입력받아 마일(mile) 단위로 변환해 출력하는 프로그램을 작성하시오.
+carspeed = int(input('>>>'))
 miles = 1.61
-carspeed = 135
-result = 135 / 1.61
+result = carspeed / miles
 
 print( carspeed,'(km)은', result, '마일(miles)이다.' )
